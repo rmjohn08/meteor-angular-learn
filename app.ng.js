928872f1d446c8d0ad334f9f7f0f762356aa3b24
Ronaldo Johnson <rmjohn08@gmail.com>
2015-09-27 20:07:15 -0500

    adding add remove capability and routes

--- app.ng.js
+++ app.ng.js
@@ -1,24 +1,59 @@
 /*
 	file rename to use the annotation ng-annotate
-	ng.js functionality.  
+	ng.js functionality.  it seems it needs to be separate
+	from app.js 
 */
 
+/*
+	for the ui-router to work need to install with meteor
+	by executing --> meteor add angularui:angular-ui-router
+*/
 if (Meteor.isClient) {
-	angular.module('socially',['angular-meteor']);
+	angular.module('socially',['angular-meteor','ui.router']);
+
+
+	angular.module('socially').config(function($urlRouterProvider,$stateProvider, $locationProvider) {
+		
+		$locationProvider.html5Mode(true);
+
+		$stateProvider
+			.state('parties', {
+				url:'/parties',
+				templateUrl: 'parties-list.ng.html',
+				controller: 'PartiesListCtrl as partyCtrl'
+
+			})
+			.state('partyDetails', {
+				url : '/parties/:partyId',
+				templateUrl : 'party-details.ng.html',
+				controller : 'PartyDetailsCtrl'
+			});
+
+			$urlRouterProvider.otherwise("/parties");
+
+
+	});
 
 	angular.module('socially').controller('PartiesListCtrl', 
-		function($scope) {
+		function($scope, $meteor) {
 
 			var partyCtrl = this;
-
-			partyCtrl.parties = [
-				{'name' : 'Dubstep-Free Zone', 'description' : 'Please no!'},
-				{'name' : 'All Dubstep', 'description' : 'Get it on!'},
-				{'name' : 'Savage laughing', 'description' : 'Leisure suit'}
-							
-			];
+			partyCtrl.parties = $meteor.collection(Parties);
 			
+			partyCtrl.addParty = function(p) {
+				partyCtrl.parties.push(p);
+				p = '';
+			}
 
+			partyCtrl.removeParty = function(p) {
+				partyCtrl.parties.remove(p);
+			}
 		}
 	);
+
+	angular.module('socially').controller('PartyDetailsCtrl', function($scope,$stateParams) {
+		$scope.partyId = $stateParams.partyId;
+	});
+
 }
+
